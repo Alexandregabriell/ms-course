36f8c265a05ed45e2dc71abd812cda5c19c1c9b1
alexandre.gabriel <alexandregabriel_@hotmail.com>
2021-11-13 10:11:54 -0300

    project hr-eureka-server created

--- hr-worker/src/main/java/com/alexandre/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/alexandre/hrworker/resources/WorkerResource.java
@@ -2,7 +2,9 @@ package com.alexandre.hrworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alexandre.hrworker.entities.Worker;
 import com.alexandre.hrworker.repositories.WorkerRepository;
 
+
 @RestController
 @RequestMapping(path = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -25,8 +33,9 @@ public class WorkerResource {
 		return ResponseEntity.ok(list);
 	}
 	
-	@GetMapping("{id}")
+	@GetMapping(value = "/{id}")
 	ResponseEntity<Worker> findById(@PathVariable Long id) {
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker worker = workerRepository.findById(id).get();
 		return ResponseEntity.ok(worker);
 	}
